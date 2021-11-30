57f81f03b1014812d20bc3271dc73df55b2c1599
Philippe Perret <philippe.perret@yahoo.fr>
2021-11-30 08:38:46 +0100

    Feature #11 (taille de l'image)

---
+++ js/app/ImgScore.js
@@ -0,0 +1,37 @@
+'use strict';
+/**
+ * constance ImgScore pour gérer l'image de la partition
+ * 
+ */
+class ImgScoreClass {
+
+constructor(){
+  this.size = 100
+}
+/**
+ * Méthodes pour aggrandir et diminuer
+ * 
+ */
+augmenteSize(){
+  this.changeSize(this.size += 5)
+}
+diminueSize(){
+  this.changeSize(this.size -= 5)
+}
+initSize(){
+  this.size = 100
+  this.changeSize(val)
+}
+changeSize(val){
+  this.img.style.width = val + '%'
+}
+
+get container(){
+  return this._conteneur || (this._conteneur = DGet('#score_container'))
+}
+get img(){
+  return this._img || (this._img = DGet('#score'))
+}
+
+}//ImgScoreClass
+const ImgScore = new ImgScoreClass()
